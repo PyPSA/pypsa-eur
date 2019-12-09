cfa435446ea21b4f0014874e6d08ce1d10ba0242
Fabian <fab.hof@gmx.de>
2019-12-09 15:40:01 +0100

    include suggestion

--- scripts/_helpers.py
+++ scripts/_helpers.py
@@ -175,10 +175,8 @@ def mock_snakemake(rulename, **wildcards):
     from snakemake.script import Snakemake
 
     script_dir = Path(__file__).parent.resolve()
-    msg = ('mock_snakemake has to be run from the repository scripts '
-           f'directory {script_dir}')
-    assert Path.cwd().resolve() == script_dir, msg
-
+    assert Path.cwd().resolve() == script_dir, \
+      f'mock_snakemake has to be run from the repository scripts directory {script_dir}'
     os.chdir(script_dir.parent)
     for p in sm.SNAKEFILE_CHOICES:
         if os.path.exists(p):
