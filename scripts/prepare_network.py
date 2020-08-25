fa605b30bf4195252fa246d50bbbe370b77ce300
Fabian <fab.hof@gmx.de>
2020-08-25 17:49:46 +0200

    adjust wildcards explanation

--- scripts/prepare_network.py
+++ scripts/prepare_network.py
@@ -9,8 +9,8 @@ Prepare PyPSA network for solving according to :ref:`opts` and :ref:`ll`, such a
 - adding an annual **limit** of carbon-dioxide emissions,
 - adding an exogenous **price** per tonne emissions of carbon-dioxide (or other kinds),
 - setting an **N-1 security margin** factor for transmission line capacities,
-- specifying a limit on the **cost** of transmission expansion,
-- specifying a limit on the **volume** of transmission expansion, and
+- specifying an expansion limit on the **cost** of transmission expansion,
+- specifying an expansion limit on the **volume** of transmission expansion, and
 - reducing the **temporal** resolution by averaging over multiple hours.
 
 Relevant Settings
