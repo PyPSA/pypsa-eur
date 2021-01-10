abc01aea253cf1d9c8d6daf0df49748dd28d7cd3
Chiara Anselmetti <40397544+chiaroo@users.noreply.github.com>
2021-01-10 14:45:19 +0100

    Update scripts/prepare_network.py
    Co-authored-by: Fabian Neumann <fabian.neumann@outlook.de>

--- scripts/prepare_network.py
+++ scripts/prepare_network.py
@@ -205,9 +205,7 @@ def enforce_autarky(n, only_crossborder=False):
                     ].index
     else:
         lines_rm = n.lines.index
-        for i in n.links.index:
-            if n.links.loc[i,'carrier'] == 'DC':
-                links_rm.append(i)
+        links_rm = n.links.loc[n.links.carrier=="DC"].index
     n.mremove("Line", lines_rm)
     n.mremove("Link", links_rm)
 
