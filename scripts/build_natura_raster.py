2b70fc19021f4b26ab4b94d90b8127842068fe6f
FabianHofmann <hofmann@fias.uni-frankfurt.de>
2021-02-19 09:55:22 +0100

    natura raster: small fixup

--- scripts/build_natura_raster.py
+++ scripts/build_natura_raster.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     transform, out_shape = get_transform_and_shape(bounds, res=100)
 
     # adjusted boundaries
-    shapes = gpd.read_file(snakemake.input.shapes).to_crs(3035)
+    shapes = gpd.read_file(snakemake.input.natura).to_crs(3035)
     raster = ~geometry_mask(shapes.geometry, out_shape[::-1], transform)
     raster = raster.astype(rio.uint8)
 
