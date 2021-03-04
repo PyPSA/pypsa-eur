e735d04ca7371ecc892eb4f7a6d899b0ed637d7f
FabianHofmann <hofmann@fias.uni-frankfurt.de>
2021-03-04 18:01:58 +0100

    Update scripts/build_cutout.py

--- scripts/build_cutout.py
+++ scripts/build_cutout.py
@@ -101,7 +101,7 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     if 'snakemake' not in globals():
         from _helpers import mock_snakemake
-        snakemake = mock_snakemake('build_cutout', cutout='base')
+        snakemake = mock_snakemake('build_cutout', cutout='europe-2013-era5')
     configure_logging(snakemake)
 
     cutout_params = snakemake.config['atlite']['cutouts'][snakemake.wildcards.cutout]
