51dffbefa393a2498b8d2fd28aaaf63a051ec1f0
Fabian Neumann <fabian.neumann@outlook.de>
2022-04-05 16:59:51 +0200

    add gas usage limit constraint

--- scripts/prepare_network.py
+++ scripts/prepare_network.py
@@ -77,6 +77,16 @@ def add_co2limit(n, co2limit, Nyears=1.):
           constant=co2limit * Nyears)
 
 
+def add_gaslimit(n, gaslimit, Nyears=1.):
+
+    sel = n.carriers.index.intersection(["OCGT", "CCGT", "CHP"])
+    n.carriers.loc[sel, "gas_usage"] = 1.
+
+    n.add("GlobalConstraint", "GasLimit",
+          carrier_attribute="gas_usage", sense="<=",
+          constant=gaslimit * Nyears)
+
+
 def add_emission_prices(n, emission_prices={'co2': 0.}, exclude_co2=False):
     if exclude_co2: emission_prices.pop('co2')
     ep = (pd.Series(emission_prices).rename(lambda x: x+'_emissions') *
@@ -236,6 +246,10 @@ if __name__ == "__main__":
             else:
                 add_co2limit(n, snakemake.config['electricity']['co2limit'], Nyears)
             break
+
+    gaslimit = snakemake.config["electricity"].get("gaslimit")
+    if gaslimit:
+        add_gaslimit(n, gaslimit, Nyears)
 
     for o in opts:
         oo = o.split("+")
