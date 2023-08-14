f5281a76f4e48c8363148e43d1f3ec985d9efc88
Fabian Neumann <fabian.neumann@outlook.de>
2023-08-14 18:25:22 +0200

    plot resources: gas network unclustered

--- rules/plot.smk
+++ rules/plot.smk
@@ -10,3 +10,19 @@ rule plot_power_network_unclustered:
         multiext(RESOURCES + "graphics/power-network-unclustered", ".png", ".pdf")
     script:
         "../scripts/plot_power_network_unclustered.py"
+
+
+rule plot_gas_network_unclustered:
+    input:
+        gas_network=RESOURCES + "gas_network.csv",
+        gem=HTTP.remote(
+            "https://globalenergymonitor.org/wp-content/uploads/2023/07/Europe-Gas-Tracker-2023-03-v3.xlsx",
+            keep_local=True,
+        ),
+        entry="data/gas_network/scigrid-gas/data/IGGIELGN_BorderPoints.geojson",
+        storage="data/gas_network/scigrid-gas/data/IGGIELGN_Storages.geojson",
+        rc="matplotlibrc",
+    output:
+        multiext(RESOURCES + "graphics/gas-network-unclustered", ".png", ".pdf")
+    script:
+        "../scripts/plot_gas_network_unclustered.py"
---
+++ scripts/plot_gas_network_unclustered.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2023- Fabian Neumann
+#
+# SPDX-License-Identifier: MIT
+"""
+Plot unclustered gas transmission network.
+"""
+
+import pandas as pd
+import numpy as np
+from shapely import wkt
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy
+from build_gas_input_locations import build_gas_input_locations
+import matplotlib.colors as mcolors
+from matplotlib.ticker import LogFormatter
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        snakemake = mock_snakemake("plot_gas_network_unclustered")
+
+    plt.style.use(snakemake.input.rc)
+
+    countries = snakemake.config["countries"]
+
+    pts = build_gas_input_locations(
+        snakemake.input.gem,
+        snakemake.input.entry,
+        snakemake.input.storage,
+        countries,
+    )
+
+    sums = pts.groupby("type").capacity.sum()
+
+    for t in ["lng", "production", "pipeline", "storage"]:
+        pts.loc[pts["type"] == t, "capacity"] /= sums[t]
+
+    pts["type"] = pts["type"].replace(
+        dict(production="Fossil Extraction", lng="LNG Terminal", pipeline="Entrypoint", storage="Storage")
+    )
+
+    df = pd.read_csv(snakemake.input.gas_network, index_col=0)
+    for col in ["geometry"]:
+        df[col] = df[col].apply(wkt.loads)
+
+    df = gpd.GeoDataFrame(df, geometry="geometry", crs="EPSG:4326")
+
+    crs = ccrs.EqualEarth()
+
+    df = df.to_crs(crs.proj4_init)
+    pts = pts.to_crs(crs.proj4_init)
+
+    fig, ax = plt.subplots(figsize=(9.5, 9.5), subplot_kw={"projection": crs})
+
+    ax.add_feature(cartopy.feature.COASTLINE.with_scale("50m"), linewidth=0.5, zorder=2)
+    ax.add_feature(cartopy.feature.BORDERS.with_scale("50m"), linewidth=0.5, zorder=2)
+
+    vmax = 100
+
+    df.plot(
+        ax=ax,
+        column=df["p_nom"].div(1e3).fillna(0.),
+        linewidths=np.log(df.p_nom.div(df.p_nom.min())).fillna(0.).div(3),
+        cmap="Spectral_r",
+        vmax=vmax,
+        legend=True,
+        legend_kwds=dict(label="Gas Pipeline Capacity [GW]", shrink=0.55, extend="max"),
+        norm=mcolors.LogNorm(vmin=1, vmax=vmax),
+    )
+
+    pts.plot(
+        ax=ax,
+        column="type",
+        markersize=pts["capacity"].fillna(0.) * 750,
+        alpha=0.8,
+        legend=True,
+        legend_kwds=dict(loc=[0.08, 0.86]),
+        zorder=6
+    )
+
+    ax.gridlines(linestyle=":")
+    ax.axis('off')
+
+    for fn in snakemake.output:
+        plt.savefig(fn)
