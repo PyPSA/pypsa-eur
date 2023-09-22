be5331c89c8aeabd64108d1c15827da47519c43a
Fabian <fab.hof@gmx.de>
2023-09-22 10:51:36 +0200

    formulate sequestration limit constraint as operational_limit constraint

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -182,9 +182,6 @@ def add_co2_sequestration_limit(n, config, limit=200):
     """
     Add a global constraint on the amount of Mt CO2 that can be sequestered.
     """
-    n.carriers.loc["co2 stored", "co2_absorptions"] = -1
-    n.carriers.co2_absorptions = n.carriers.co2_absorptions.fillna(0)
-
     limit = limit * 1e6
     for o in opts:
         if "seq" not in o:
@@ -396,7 +393,7 @@ def prepare_network(
         if snakemake.params["sector"]["limit_max_growth"]["enable"]:
             n = add_max_growth(n, config)
 
-    if n.stores.carrier.eq("co2 stored").any():
+    if n.stores.carrier.eq("co2 sequestered").any():
         limit = co2_sequestration_potential
         add_co2_sequestration_limit(n, config, limit=limit)
 
