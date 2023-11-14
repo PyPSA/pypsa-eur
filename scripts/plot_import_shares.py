0c1274a348f4d5828165459cc28574c308c46a3a
Fabian Neumann <fabian.neumann@outlook.de>
2023-11-14 17:56:58 +0100

    plot_import_shares: add import mix as subplot

--- scripts/plot_import_shares.py
+++ scripts/plot_import_shares.py
@@ -11,6 +11,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import pypsa
 from _helpers import configure_logging
 
@@ -24,6 +25,16 @@ CARRIERS = {
     "steel": "steel",
 }
 
+COLOR_MAPPING = {
+    "electricity": "import hvdc-to-elec",
+    "hydrogen": "import pipeline-h2",
+    "ammonia": "import shipping-lnh3",
+    "methane": "import shipping-lch4",
+    "methanol": "import shipping-meoh",
+    "Fischer-Tropsch": "import shipping-ftfuel",
+    "steel": "import shipping-steel",
+}
+
 THRESHOLD = 1 # MWh
 
 if __name__ == "__main__":
@@ -34,56 +45,92 @@ if __name__ == "__main__":
             "plot_import_shares",
             simpl="",
             opts="",
-            clusters="100",
+            clusters="110",
             ll="vopt",
-            sector_opts="Co2L0-2190SEG-T-H-B-I-S-A-imp",
+            sector_opts="Co2L0-2190SEG-T-H-B-I-S-A-onwind+p0.5-imp",
             planning_horizons="2050",
-            configfiles="../../config/config.20230926-zecm.yaml",
+            configfiles="../../config/config.20231025-zecm.yaml",
         )
 
     configure_logging(snakemake)
 
     plt.style.use(['bmh', snakemake.input.rc])
 
+    tech_colors = snakemake.config["plotting"]["tech_colors"]
+
     n = pypsa.Network(snakemake.input.network)
 
     eb = n.statistics.energy_balance().groupby(["carrier", "bus_carrier"]).sum()
-    supply = eb.where(eb > THRESHOLD).dropna().div(1e6)
+    eb = eb.unstack(1).groupby(lambda x: "import" if "import" in x or "external" in x else x).sum()
+    supply = eb.where(eb > THRESHOLD).dropna(how='all', axis=0).div(1e6)
 
-    ie = supply.unstack(1).groupby(lambda x: "import" if "import" in x or "external" in x else "domestic").sum()
-    ie = ie / ie.sum() * 100
+    ie = supply.groupby(lambda x: "import" if x == "import" else "domestic").sum()
+    ie_rel = ie / ie.sum() * 100
+    ie_sum = ie.sum().astype(int)
 
+    imp_mix = ie.loc["import"].copy()
+    if "steel" in imp_mix.index:
+        imp_mix.loc["steel"] *= 2.1 # kWh/kg
+    imp_mix = pd.DataFrame(imp_mix.where(imp_mix > 1e-3).dropna().rename(index=CARRIERS).sort_values(ascending=False))
+    imp_mix.columns = ["import mix"]
 
-    fig, ax = plt.subplots(figsize=(6, 4))
+    fig, (ax, ax_mix) = plt.subplots(2, 1, figsize=(6, 5), gridspec_kw=dict(height_ratios=[5.5, 1]))
     sel = list(CARRIERS.keys())[::-1]
-    ie[sel].rename(columns=CARRIERS).T.plot.barh(
+    ie_rel[sel].rename(columns=CARRIERS).T.plot.barh(
         stacked=True,
         ax=ax,
         color=['lightseagreen', 'coral']
     )
 
-    plt.ylabel("")
-    plt.xlabel("domestic share [%]", fontsize=11, color='lightseagreen')
-    plt.legend(ncol=2, bbox_to_anchor=(0.35, 1.25))
-    plt.grid(axis="y")
-    plt.xlim(0,100)
+    ax.set_ylabel("")
+    ax.set_xlabel("domestic share [%]", fontsize=11, color='lightseagreen')
+    ax.legend(ncol=2, bbox_to_anchor=(0.35, 1.25))
+    ax.grid(axis="y")
+    ax.set_xlim(0,100)
+
+    imp_mix.T.plot.barh(ax=ax_mix, stacked=True, legend=True, color=[tech_colors[COLOR_MAPPING[i]]for i in imp_mix.index])
+
+    for i, (carrier, twh) in enumerate(ie_sum[sel].items()):
+        unit = "Mt" if carrier.lower().startswith("steel") else "TWh"
+        ax.text(119, i, f"{twh} {unit}", va='center', ha='right', color='slateblue')
+    ax.text(119, i + 1.5, "total\nsupply", va='center', ha='right', color='slateblue')
 
     secax = ax.secondary_xaxis('top', functions=(lambda x: 100 - x, lambda x: 100 -x))
     secax.set_xlabel('import share [%]', fontsize=11, color='coral')
 
+    total_imp = imp_mix.sum().sum()
+    secax_mix = ax_mix.secondary_xaxis('top', functions=(lambda x: x / total_imp * 100, lambda x: x * total_imp * 100))
+
+
+    ax_mix.text(total_imp * 1.1, -0.75, "TWh", va='center')
+    ax_mix.text(total_imp * 1.1, 0.75, "%", va='center')
+    ax_mix.legend(ncol=4, bbox_to_anchor=(1.15, -0.3), title="")
+
     ticks = range(10, 100, 20)
     ax.set_xticks(ticks, minor=True)
     secax.set_xticks(ticks, minor=True)
+    secax_mix.set_xticks(ticks, minor=True)
+
+    ax_mix.set_xlim(0, total_imp)
+    ax_mix.grid(axis="y")
 
     for i in ["top", "right", "left", "bottom"]:
         secax.spines[i].set_visible(False)
         ax.spines[i].set_visible(False)
+        ax_mix.spines[i].set_visible(False)
+        secax_mix.spines[i].set_visible(False)
 
     def fmt(x):
         return f"{x:.0f}%" if x > 1 else ""
 
     for container in ax.containers[:2]:
         ax.bar_label(container, label_type='center', color='white', fmt=fmt)
+
+    def fmt(x):
+        return f"{x:.0f}" if x > 200 else ""
+
+    for container in ax_mix.containers:
+        ax_mix.bar_label(container, label_type='center', color='white', fmt=fmt)
 
     for fn in snakemake.output:
         plt.savefig(fn, bbox_inches="tight")
