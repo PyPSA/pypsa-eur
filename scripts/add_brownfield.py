73abb6196796ae0d741050f5cfe333b8c02cd768
Fabian Neumann <fabian.neumann@outlook.de>
2024-01-04 17:05:54 +0100

    add_brownfield: fix lv_limit reached

--- scripts/add_brownfield.py
+++ scripts/add_brownfield.py
@@ -134,7 +134,7 @@ def disable_grid_expansion_if_LV_limit_hit(n):
     # allow small numerical differences
     if lv_limit - total_expansion < 1:
         logger.info(
-            f"LV is already reached (gap {diff} MWkm), disabling expansion and LV limit"
+            f"LV is already reached, disabling expansion and LV limit"
         )
         extendable_acs = n.lines.query("s_nom_extendable").index
         n.lines.loc[extendable_acs, "s_nom_extendable"] = False
