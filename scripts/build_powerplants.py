61211eb87ba0e484536308ec70fc8a4eb3a31ff6
euronion <42553970+euronion@users.noreply.github.com>
2024-01-08 09:45:14 +0100

    Fix variable name

--- scripts/build_powerplants.py
+++ scripts/build_powerplants.py
@@ -110,7 +110,7 @@ def add_custom_powerplants(ppl, custom_powerplants, custom_ppl_query=False):
 
 def add_everywhere_powerplants(ppl, substations, everywhere_powerplants):
     # Create a dataframe with "everywhere_powerplants" of stated carriers at the location of all substations
-    generic_ppl = (
+    everywhere_ppl = (
         pd.DataFrame(
             itertools.product(substations.index.values, everywhere_powerplants),
             columns=["substation_index", "Fueltype"],
