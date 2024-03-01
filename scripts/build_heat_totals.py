d2527ed0ad1b48281949cb75d6d2b8cad9348da1
Koen van Greevenbroek <koen.v.greevenbroek@uit.no>
2024-03-01 13:30:01 +0100

    Add copyright headers

--- scripts/build_heat_totals.py
+++ scripts/build_heat_totals.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2017-2024 The PyPSA-Eur Authors
+#
+# SPDX-License-Identifier: MIT
 """
 Approximate heat demand for all weather years.
 """
--- scripts/solve_operations_network_other_year_myopic.py
+++ scripts/solve_operations_network_other_year_myopic.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: : 2017-2024 The PyPSA-Eur Authors
+#
+# SPDX-License-Identifier: MIT
 """
 Solve myopic operations network.
 """
