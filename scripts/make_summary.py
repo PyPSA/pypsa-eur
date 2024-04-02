adc805e2c59cdb2d6833ac44dc4e69276ec39bff
lisazeyen <lisa.zeyen@web.de>
2024-04-02 12:01:40 +0200

    add try except when read in

--- scripts/make_summary.py
+++ scripts/make_summary.py
@@ -652,8 +652,11 @@ def make_summaries(networks_dict):
     df = {output: pd.DataFrame(columns=columns, dtype=float) for output in outputs}
     for label, filename in networks_dict.items():
         logger.info(f"Make summary for scenario {label}, using {filename}")
-
-        n = pypsa.Network(filename)
+        try:
+            n = pypsa.Network(filename)
+        except FileNotFoundError:
+            logger.info(f"{label} not yet solved.")
+            continue
         
         if not hasattr(n, "objective"): continue
         assign_carriers(n)
