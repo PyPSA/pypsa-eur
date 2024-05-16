0a6867fe0be9df660da6e6d1c2f55f35beb466dc
cpschau <caspar.schauss@ieg.fraunhofer.de>
2024-05-16 14:14:13 +0200

    add network components

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -70,6 +70,19 @@ def define_spatial(nodes, options):
 
     spatial.biomass.df = pd.DataFrame(vars(spatial.biomass), index=nodes)
 
+    # municipal solid waste
+
+    if options.get("waste_incineration"):
+        spatial.waste = SimpleNamespace()
+        if options.get("waste_incineration") == "regional":
+            spatial.waste.nodes = nodes + " municipal waste"
+            spatial.waste.locations = nodes
+        else:
+            spatial.waste.nodes = ["EU municipal waste"]
+            spatial.waste.locations = ["EU"]
+
+        spatial.waste.df = pd.DataFrame(vars(spatial.waste), index=nodes)
+
     # co2
 
     spatial.co2 = SimpleNamespace()
@@ -2244,18 +2257,54 @@ def add_biomass(n, costs):
         biogas_potentials_spatial = biomass_potentials["biogas"].rename(
             index=lambda x: x + " biogas"
         )
+        unsustainable_biogas_potentials_spatial = biomass_potentials[
+            "unsustainable biogas"
+        ].rename(index=lambda x: x + " biogas")
     else:
         biogas_potentials_spatial = biomass_potentials["biogas"].sum()
+        unsustainable_biogas_potentials_spatial = biomass_potentials[
+            "unsustainable biogas"
+        ].sum()
 
     if options.get("biomass_spatial", options["biomass_transport"]):
         solid_biomass_potentials_spatial = biomass_potentials["solid biomass"].rename(
             index=lambda x: x + " solid biomass"
         )
+        unsustainable_solid_biomass_potentials_spatial = biomass_potentials[
+            "unsustainable solid biomass"
+        ].rename(index=lambda x: x + " solid biomass")
+
     else:
         solid_biomass_potentials_spatial = biomass_potentials["solid biomass"].sum()
+        unsustainable_solid_biomass_potentials_spatial = biomass_potentials[
+            "unsustainable solid biomass"
+        ].sum()
+
+    if options["regional_oil_demand"]:
+        unsustainable_liquid_biofuel_potentials_spatial = biomass_potentials[
+            "unsustainable liquid biofuels"
+        ].rename(index=lambda x: x + " liquid biofuels")
+    else:
+        unsustainable_liquid_biofuel_potentials_spatial = biomass_potentials[
+            "unsustainable liquid biofuels"
+        ].sum()
+
+    if options.get("waste_incineration") == "regional":
+        waste_potentials_spatial = biomass_potentials["waste"].rename(
+            index=lambda x: x + " municipal waste"
+        )
+        unsustainable_waste_potentials_spatial_ = biomass_potentials["waste"].rename(
+            index=lambda x: x + " municipal waste"
+        )
+    else:
+        waste_potentials_spatial = biomass_potentials["waste"].sum()
+        unsustainable_waste_potentials_spatial_ = biomass_potentials[
+            "unsustainable waste"
+        ].sum()
 
     n.add("Carrier", "biogas")
     n.add("Carrier", "solid biomass")
+    n.add("Carrier", "municipal waste")
 
     n.madd(
         "Bus",
@@ -2274,6 +2323,14 @@ def add_biomass(n, costs):
     )
 
     n.madd(
+        "Bus",
+        spatial.waste.nodes,
+        location=spatial.waste.locations,
+        carrier="municipal waste",
+        unit="LHV",
+    )
+
+    n.madd(
         "Store",
         spatial.gas.biogas,
         bus=spatial.gas.biogas,
@@ -2285,6 +2342,17 @@ def add_biomass(n, costs):
 
     n.madd(
         "Store",
+        spatial.gas.biogas,
+        suffix=" unsustainable",
+        bus=spatial.gas.biogas,
+        carrier="unsustainable biogas",
+        e_nom=unsustainable_biogas_potentials_spatial,
+        marginal_cost=costs.at["biogas", "fuel"],
+        e_initial=unsustainable_biogas_potentials_spatial,
+    )
+
+    n.madd(
+        "Store",
         spatial.biomass.nodes,
         bus=spatial.biomass.nodes,
         carrier="solid biomass",
@@ -2292,6 +2360,65 @@ def add_biomass(n, costs):
         marginal_cost=costs.at["solid biomass", "fuel"],
         e_initial=solid_biomass_potentials_spatial,
     )
+
+    n.madd(
+        "Store",
+        spatial.biomass.nodes,
+        suffix=" unsustainable",
+        bus=spatial.biomass.nodes,
+        carrier="unsustainable solid biomass",
+        e_nom=unsustainable_solid_biomass_potentials_spatial,
+        marginal_cost=costs.at["solid biomass", "fuel"],
+        e_initial=unsustainable_solid_biomass_potentials_spatial,
+    )
+
+    n.madd(
+        "Store",
+        spatial.oil.demand_locations,
+        suffix=" unsustainable liquid biofuel",
+        bus=spatial.oil.demand_locations + " liquid biofuel",
+        carrier="unsustainable liquid biofuels",
+        e_nom=unsustainable_liquid_biofuel_potentials_spatial,
+        marginal_cost=costs.at["oil", "fuel"],
+        e_initial=unsustainable_liquid_biofuel_potentials_spatial,
+    )
+
+    # Create a unidirectional Link from the liquid STore to the oil bus with a connection to the atmosphere uswing the BtL CO2 intensity
+    n.madd(
+        "Link",
+        spatial.oil.demand_locations,
+        suffix=" liquid biofuel",
+        bus0=spatial.oil.demand_locations + " liquid biofuel",
+        bus1=spatial.oil.nodes,
+        bus2="co2 atmosphere",
+        carrier="liquid biofuels",
+        efficiency=1,
+        efficiency2=-costs.at["solid biomass", "CO2 intensity"]
+        + costs.at["BtL", "CO2 stored"],
+        p_nom_extendable=True,
+    )
+
+    if options.get("waste_incineration") != "False":
+        n.madd(
+            "Store",
+            spatial.waste.nodes,
+            bus=spatial.waste.nodes,
+            carrier="municipal waste",
+            e_nom=waste_potentials_spatial,
+            marginal_cost=0,  # main purpose is not the energy but the waste disposal
+            e_initial=waste_potentials_spatial,
+        )
+
+        n.madd(
+            "Store",
+            spatial.waste.nodes,
+            suffix=" unsustainable",
+            bus=spatial.waste.nodes,
+            carrier="unsustainable municipal waste",
+            e_nom=unsustainable_waste_potentials_spatial_,
+            marginal_cost=0,  # main purpose is not the energy but the waste disposal
+            e_initial=unsustainable_waste_potentials_spatial_,
+        )
 
     n.madd(
         "Link",
@@ -2448,6 +2575,58 @@ def add_biomass(n, costs):
             * costs.at["biomass CHP capture", "capture_rate"],
             lifetime=costs.at[key, "lifetime"],
         )
+
+        if options["waste_incineration"]:
+
+            n.madd(
+                "Link",
+                urban_central,
+                suffix=" urban central waste CHP",
+                bus0=spatial.waste.df.loc[urban_central, "nodes"].values,
+                bus1=urban_central,
+                bus2=urban_central + " urban central heat",
+                carrier="urban central waste CHP",
+                efficiency=costs.at["waste CHP", "efficiency"],
+                efficiency2=costs.at["waste CHP", "efficiency-heat"],
+                p_nom_extendable=True,
+                p_min_pu=1,  # waste CHPs are usually run at a high load
+                capital_cost=costs.at["waste CHP", "fixed"],
+                marginal_cost=costs.at["waste CHP", "VOM"],
+                lifetime=costs.at["waste CHP", "lifetime"],
+            )
+
+            n.madd(
+                "Link",
+                urban_central,
+                suffix=" urban central waste CHP CC",
+                bus0=spatial.waste.df.loc[urban_central, "nodes"].values,
+                bus1=urban_central,
+                bus2=urban_central + " urban central heat",
+                bus3="co2 atmosphere",
+                bus4=spatial.co2.df.loc[urban_central, "nodes"].values,
+                carrier="urban central waste CHP CC",
+                efficiency=costs.at["waste CHP CC", "efficiency"]
+                - costs.at["solid biomass", "CO2 intensity"]
+                * (
+                    costs.at["biomass CHP capture", "electricity-input"]
+                    + costs.at["biomass CHP capture", "compression-electricity-input"]
+                ),
+                efficiency2=costs.at["waste CHP CC", "efficiency-heat"]
+                + costs.at["solid biomass", "CO2 intensity"]
+                * (
+                    costs.at["biomass CHP capture", "heat-output"]
+                    + costs.at["biomass CHP capture", "compression-heat-output"]
+                    - costs.at["biomass CHP capture", "heat-input"]
+                ),
+                efficiency3=-costs.at["solid biomass", "CO2 intensity"]
+                * costs.at["biomass CHP capture", "capture_rate"],
+                efficiency4=costs.at["solid biomass", "CO2 intensity"]
+                * costs.at["biomass CHP capture", "capture_rate"],
+                p_nom_extendable=True,
+                capital_cost=costs.at["waste CHP CC", "fixed"],
+                marginal_cost=costs.at["waste CHP CC", "VOM"],
+                lifetime=costs.at["waste CHP CC", "lifetime"],
+            )
 
     if options["biomass_boiler"]:
         # TODO: Add surcharge for pellets
@@ -3756,6 +3935,9 @@ if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
 
+        import os
+
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
         snakemake = mock_snakemake(
             "prepare_sector_network",
             # configfiles="test/config.overnight.yaml",
