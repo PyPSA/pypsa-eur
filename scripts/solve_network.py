d7435b6214a6fb27415730646a3eab72c46b279f
Sermisha <sermisha.narayana@openenergytransition.org>
2024-05-20 14:57:18 +0530

    Modification to function "add_operational_reserve_margin" in solve_network.py
    Typecasted pandas dataframes to xarray datasets to allow for multiplication with pypsa variables

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -735,7 +735,7 @@ def add_operational_reserve_margin(n, sns, config):
             .loc[vres_i.intersection(ext_i)]
             .rename({"Generator-ext": "Generator"})
         )
-        lhs = summed_reserve + (p_nom_vres * (-EPSILON_VRES * capacity_factor)).sum(
+        lhs = summed_reserve + (p_nom_vres * (-EPSILON_VRES * xr.DataArray(capacity_factor))).sum(
             "Generator"
         )
 
@@ -767,7 +767,7 @@ def add_operational_reserve_margin(n, sns, config):
 
     p_max_pu = get_as_dense(n, "Generator", "p_max_pu")
 
-    lhs = dispatch + reserve - capacity_variable * p_max_pu[ext_i]
+    lhs = dispatch + reserve - capacity_variable * xr.DataArray(p_max_pu[ext_i])
 
     rhs = (p_max_pu[fix_i] * capacity_fixed).reindex(columns=gen_i, fill_value=0)
 
