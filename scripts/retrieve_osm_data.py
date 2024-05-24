ff882e69f9aedb4e5aa9f3ecb399e0c85f654355
Bobby Xiong <bobbyxng@gmail.com>
2024-05-24 14:14:26 +0200

    Added links_relations (route = power, frequency = 0) to retrieval. This will change how HVDC links are extracted in the near future.

--- scripts/retrieve_osm_data.py
+++ scripts/retrieve_osm_data.py
@@ -22,79 +22,13 @@ from _helpers import configure_logging
 logger = logging.getLogger(__name__)
 
 
-# Function currently not needed - Kept for backup purposes to retrieve the OSM
-# area code if needed in the future
-def _get_overpass_areas(countries):
-    """
-    Retrieve the OSM area codes for the specified country codes.
-
-    Parameters
-    ----------
-    countries : str or list
-        A single country code or a list of country codes for which the OSM area
-        codes should be retrieved.
-
-    Returns
-    -------
-    dict
-        A dictionary mapping country codes to their corresponding OSM area
-        codes.
-    """
-
-    # If a single country code is provided, convert it to a list
-    if not isinstance(countries, list):
-        countries = [countries]
-
-    # Overpass API endpoint URL
-    overpass_url = "https://overpass-api.de/api/interpreter"
-
-    osm_areas = []
-    for c in countries:
-        # Overpass query to fetch the relation for the specified country code
-        overpass_query = f"""
-            [out:json];
-            area["ISO3166-1"="{c}"];
-            out;
-        """
-
-        # Send the request to Overpass API
-        response = requests.post(overpass_url, data=overpass_query)
-
-        try:
-            # Parse the response
-            data = response.json()
-
-            # Check if the response contains any results
-            if "elements" in data and len(data["elements"]) > 0:
-                # Extract the area ID from the relation
-                if c == "FR":  # take second one for France
-                    osm_area_id = data["elements"][1]["id"]
-                else:
-                    osm_area_id = data["elements"][0]["id"]
-                osm_areas.append(f"area({osm_area_id})")
-            else:
-                # Print a warning if no results are found for the country code
-                logger.info(
-                    f"No area code found for the specified country "
-                    f"code: {c}. Omitted from the list."
-                )
-        except json.JSONDecodeError as e:
-            logger.error(f"JSON decode error for country {c}: {e}")
-            logger.debug(f"Response text: {response.text}")
-
-    # Create a dictionary mapping country codes to their corresponding OSM area
-    # codes
-    op_areas_dict = dict(zip(countries, osm_areas))
-
-    return op_areas_dict
-
-
 def retrieve_osm_data(
     country,
     output,
     features=[
         "cables_way",
         "lines_way",
+        "links_relation",
         "substations_way",
         "substations_relation",
     ],
@@ -121,23 +55,10 @@ def retrieve_osm_data(
     # Overpass API endpoint URL
     overpass_url = "https://overpass-api.de/api/interpreter"
 
-    # More features can in theory be retrieved that are currently not needed
-    # to build a functioning network. The following power-related
-    # features are supported:
-
-    # features_dict= {
-    #     'cables_way': 'way["power"="cable"]',
-    #     'lines_way': 'way["power"="line"]',
-    #     'substations_way': 'way["power"="substation"]',
-    #     'substations_node': 'node["power"="substation"]',
-    #     'transformers_way': 'way["power"="transformer"]',
-    #     'transformers_node': 'node["power"="transformer"]',
-    #     'route_relations': 'rel["route"="power"]["type"="route"]'
-    # }
-
     features_dict = {
         "cables_way": 'way["power"="cable"]',
         "lines_way": 'way["power"="line"]',
+        "links_relation": 'relation["route"="power"]["frequency"="0"]',
         "substations_way": 'way["power"="substation"]',
         "substations_relation": 'relation["power"="substation"]',
     }
