ed52741639fdeea15ff07719ea9e7c9a8aab3a56
martacki <martha.frysztacki@gmail.com>
2024-05-28 15:20:02 +0200

    reduce dispatch of nucelar to 70%

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -860,6 +860,7 @@ def add_generation(n, costs, existing_capacities=0, existing_efficiencies=None):
                 existing_capacities[generator] / costs.at[generator, "efficiency"]
                 if not existing_capacities == 0 else 0
             ), # NB: existing capacities are MWel
+            p_max_pu = 0.7 if carrier == "uranium" else 1, # be conservative for nuclear (maintance or unplanned shut downs)
             p_nom_min=(
                 existing_capacities[generator] if not existing_capacities == 0 else 0
             ),
