1d588e2bb203d7bec3f86819f9847a579ecf1958
toniseibold <tseibold.tub@gmail.com>
2024-06-03 10:50:52 +0200

    documentation for industry rules

--- scripts/build_ammonia_production.py
+++ scripts/build_ammonia_production.py
@@ -4,6 +4,21 @@
 # SPDX-License-Identifier: MIT
 """
 Build historical annual ammonia production per country in ktonNH3/a.
+
+Inputs
+-------
+
+- ``data/bundle-sector/myb1-2017-nitro.xls``
+
+Outputs
+-------
+
+- ``resources/ammonia_production.csv``
+
+Description
+-------
+
+This functions takes data from the `Minerals Yearbook <https://www.usgs.gov/centers/national-minerals-information-center/nitrogen-statistics-and-information>`_ (June 2024) published by the US Geological Survey (USGS) and the National Minerals Information Center and extracts the annual ammonia production per country in ktonN/a. The data is converted to ktonNH3/a.
 """
 
 import country_converter as coco
