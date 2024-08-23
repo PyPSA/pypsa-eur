6957eecdd7b694012a73cf9dff99416a5645b46a
bobbyxng <bobbyxng@gmail.com>
2024-08-23 09:45:58 +0200

    removed run parameter in mock_snakemake (only used for debugging).

--- scripts/cluster_network.py
+++ scripts/cluster_network.py
@@ -462,7 +462,7 @@ if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
 
-        snakemake = mock_snakemake("cluster_network", simpl="", clusters="40", run = "osm-prebuilt_nuts2")
+        snakemake = mock_snakemake("cluster_network", simpl="", clusters="40")
     configure_logging(snakemake)
     set_scenario_config(snakemake)
 
