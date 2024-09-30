f1f0d20b5ff2e4d269c41bb301e87765f84d119c
Amos Schledorn <60692940+amos-schledorn@users.noreply.github.com>
2024-09-30 10:42:47 +0200

    Update scripts/build_central_heating_temperature_profiles/run.py
    Co-authored-by: Fabian Neumann <fabian.neumann@outlook.de>

--- scripts/build_central_heating_temperature_profiles/run.py
+++ scripts/build_central_heating_temperature_profiles/run.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
     max_forward_temperature_investment_year = scale_temperature_to_investment_year(
         temperature_baseyear=snakemake.params.max_forward_temperature_central_heating_baseyear,
         relative_annual_temperature_reduction=snakemake.params.relative_annual_temperature_reduction,
-        investment_year=int(snakemake.wildcards.planning_horizons[-4:]),
+        investment_year=int(snakemake.wildcards.planning_horizons),
         current_year=int(snakemake.params.energy_totals_year),
     )
 
