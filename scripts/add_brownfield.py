7f9fb106d78f6ea214686caed6cc930dd2c754cb
Michael Lindner <michaellindner@posteo.de>
2024-10-15 12:03:04 +0200

    overwrite p_nom, s_nom for power lines with *nom_opt from previous period

--- scripts/add_brownfield.py
+++ scripts/add_brownfield.py
@@ -30,8 +30,10 @@ def add_brownfield(n, n_p, year):
 
     # electric transmission grid set optimised capacities of previous as minimum
     n.lines.s_nom_min = n_p.lines.s_nom_opt
+    n.lines.s_nom = n_p.lines.s_nom_opt
     dc_i = n.links[n.links.carrier == "DC"].index
     n.links.loc[dc_i, "p_nom_min"] = n_p.links.loc[dc_i, "p_nom_opt"]
+    n.links.loc[dc_i, "p_nom"] = n_p.links.loc[dc_i, "p_nom_opt"]
 
     for c in n_p.iterate_components(["Link", "Generator", "Store"]):
         attr = "e" if c.name == "Store" else "p"
