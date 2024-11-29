f7b5a846f0b2c1e99cd6913dcccbc985b3bb25a5
Fabrizio Finozzi <fabrizio.finozzi.business@gmail.com>
2024-11-29 13:37:43 +0100

    code: add further unit tests base_network

--- test/test_base_network.py
+++ test/test_base_network.py
@@ -17,9 +17,72 @@ import pytest
 
 sys.path.append("./scripts")
 
-from base_network import _get_country, _get_oid
+from base_network import _get_country, _get_linetypes_config, _get_linetype_by_voltage, _get_oid
 
 path_cwd = pathlib.Path.cwd()
+
+
+@pytest.mark.parametrize(
+    "column_name, expected",
+    [
+        ("tags", pd.Series(["NG", "CH", "AU"])),
+        ("other", pd.Series([np.nan, np.nan, np.nan])),
+    ],
+)
+def test_get_country(column_name, expected):
+    """
+    Verify what returned by _get_country()
+    """
+    data_list = [['"country"=>"NG"'], ['"country"=>"CH"'], ['"country"=>"AU"']]
+    df_exercise = pd.DataFrame(data_list, columns=[column_name])
+    output_series = _get_country(df_exercise)
+    comparison_series = output_series.compare(expected)
+    assert comparison_series.size == 0
+
+
+def test_get_linetypes_config(config):
+    """
+    Verify what returned by _get_linetypes_config.
+    """
+    output_dict = _get_linetypes_config(
+        config["lines"]["types"], config["electricity"]["voltages"]
+    )
+    assert output_dict == config["lines"]["types"]
+
+
+def test_get_linetype_by_voltage(config):
+    """
+    Verify what returned by _get_linetype_by_voltage.
+    """
+
+    reference_list = [
+        "Al/St 240/40 2-bundle 220.0",
+        "Al/St 240/40 3-bundle 300.0",
+        "Al/St 240/40 3-bundle 300.0",
+        "Al/St 240/40 4-bundle 380.0",
+        "Al/St 240/40 4-bundle 380.0",
+        "Al/St 240/40 4-bundle 380.0",
+        "Al/St 560/50 4-bundle 750.0"
+    ]
+
+    v_nom_list = [
+        220.0,
+        300.0,
+        330.0,
+        380.0,
+        400.0,
+        500.0,
+        750.0,
+    ]
+
+    line_type_list = []
+
+    for v_nom in v_nom_list:
+        line_type_list.append(
+            _get_linetype_by_voltage(v_nom, config["lines"]["types"])
+        )
+    assert len(line_type_list) == len(reference_list)
+    assert all([x == y for x, y in zip(line_type_list, reference_list)])
 
 
 @pytest.mark.parametrize(
@@ -37,23 +100,5 @@ def test_get_oid(column_name, expected):
     df_exercise = pd.DataFrame(data_list, columns=[column_name])
     output_series = _get_oid(df_exercise)
     print(output_series)
-    comparison_series = output_series.compare(expected)
-    assert comparison_series.size == 0
-
-
-@pytest.mark.parametrize(
-    "column_name, expected",
-    [
-        ("tags", pd.Series(["NG", "CH", "AU"])),
-        ("other", pd.Series([np.nan, np.nan, np.nan])),
-    ],
-)
-def test_get_country(column_name, expected):
-    """
-    Verify what returned by _get_country()
-    """
-    data_list = [['"country"=>"NG"'], ['"country"=>"CH"'], ['"country"=>"AU"']]
-    df_exercise = pd.DataFrame(data_list, columns=[column_name])
-    output_series = _get_country(df_exercise)
     comparison_series = output_series.compare(expected)
     assert comparison_series.size == 0
