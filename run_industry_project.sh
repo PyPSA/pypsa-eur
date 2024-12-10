e35799578d0ae65279d77302a437264e529c24fb
Alice Di Bella <ad05922@login1.juno.cmcc.scc>
2024-12-10 17:19:44 +0100

    Having different config file per runs

--- run_industry_project.sh
+++ run_industry_project.sh
@@ -5,4 +5,7 @@ source /work/cmcc/ad05922/conda_startup
 conda activate pypsa-eur
 
 # Submit the job to the batch system
-bsub -q p_long -M 32G -n 1 -P 0607 snakemake -call all --configfile=config/industry_config.yaml
+bsub -q p_long -M 32G -n 1 -P 0607 -I snakemake -call all --configfile=config/industry_config_base_eu.yaml
+bsub -q p_long -M 32G -n 1 -P 0607 snakemake -call all --configfile=config/industry_config_base_reg.yaml
+bsub -q p_long -M 32G -n 1 -P 0607 snakemake -call all --configfile=config/industry_config_pol_eu.yaml
+bsub -q p_long -M 32G -n 1 -P 0607 snakemake -call all --configfile=config/industry_config_pol_reg.yaml
