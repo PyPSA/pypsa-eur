6efd49c06946ff156e4007dbd542021f15ec31b1
Amos Schledorn <a.schledorn@tu-berlin.de>
2025-01-27 10:56:42 +0100

    docs: update docs

--- scripts/build_geothermal_heat_potential.py
+++ scripts/build_geothermal_heat_potential.py
@@ -4,7 +4,7 @@
 """
 Build heat source potentials for a given heat source.
 
-This script maps and aggregates heat source potentials per heat source to `onshore_regions` using `OnshoreRegionData`.
+This script maps and aggregates geothermal heat source potentials `onshore_regions`. Input data is provided on LAU-level and is aggregated to the onshore regions.
 It scales the heat source utilisation potentials to technical potentials by dividing the utilisation potentials by the full load hours of the heat source, also taking into account the energy unit set for the respective source in the config.
 
 
@@ -13,23 +13,32 @@ Relevant Settings
 .. code:: yaml
     sector:
         district_heating:
-            heat_utilisation_potentials:
-    {heat_source}
-
+            limited_heat_sources:
+                geothermal:
+                    constant_temperature_celsius
 
 Inputs
 ------
 - `resources/<run_name>/regions_onshore.geojson`
-- `resources/<run_name>/heat_source_utilisation_potentials/<heat_source>.gpkg`
+- `resources/<run_name>/lau_regions.geojson`
+- `resources/<run_name>/isi_heat_potentials.xlsx`
 
 Outputs
 -------
 - `resources/<run_name>/heat_source_technical_potential_{heat_source}_base_s_{clusters}.csv`
+
+Raises
+------
+- ValueError if some LAU regions in ISI heat potentials are missing from the LAU Regions data.
+
+Source
+----------
+- Manz et al. 2024: "Spatial analysis of renewable and excess heat potentials for climate-neutral district heating in Europe", Renewable Energy, vol. 224, no. 120111, https://doi.org/10.1016/j.renene.2024.120111
 """
 
 import geopandas as gpd
 import pandas as pd
-from _helpers import get_snapshots, set_scenario_config
+from _helpers import set_scenario_config
 
 ISI_TEMPERATURE_SCENARIOS = {
     65: "low_temperatures",
@@ -79,13 +88,13 @@ def get_unit_conversion_factor(
 
 def get_heat_source_power(
     regions_onshore: gpd.GeoDataFrame,
-    geothermal_potentials: gpd.GeoDataFrame,
+    supply_potentials: gpd.GeoDataFrame,
     full_load_hours: float,
     input_unit: str,
     output_unit: str = "MWh",
 ) -> pd.DataFrame:
     """
-    Get the heat source power from utilisation potentials.
+    Get the heat source power from supply potentials.
 
     Note
     ----
@@ -93,25 +102,21 @@ def get_heat_source_power(
 
     Parameters
     ----------
-    snapshots : pd.DatetimeIndex
-        Datetime index of the snapshots.
     regions_onshore : gpd.GeoDataFrame
         GeoDataFrame of the onshore regions.
-    heat_source_utilisation_potential : gpd.GeoDataFrame
-        GeoDataFrame of the heat source utilisation potentials.
-    column_name : str
-        Name of the relevant column in `heat_source_utilisation_potentials`.
+    supply_potentials : gpd.GeoDataFrame
+        GeoDataFrame of the heat source supply potentials.
     full_load_hours : float
-        Full load hours assumed in the utilisation potential computation. Used to scale the utilisation potentials to technical potentials.
+        Full load hours assumed in the supply potential computation. Used to scale the supply potentials to technical potentials.
     input_unit : str
-        Unit of the utilisation potentials. Used to convert to the output unit.
+        Unit of the supply potentials. Used to convert to the output unit.
     output_unit : str, optional
         Unit of the technical potentials. Default: "MWh".
 
     Returns
     -------
     pd.DataFrame
-        Heat source power in the onshore regions. Indexed by time and name (onshore region).
+        Heat source power in the onshore regions. Indexed by name (onshore region).
     """
 
     unit_conversion_factor = get_unit_conversion_factor(
@@ -121,8 +126,8 @@ def get_heat_source_power(
     scaling_factor = unit_conversion_factor / full_load_hours
 
     heat_potentials_in_lau = gpd.GeoDataFrame(
-        geothermal_potentials,
-        geometry=lau_regions.geometry[geothermal_potentials.index],
+        supply_potentials,
+        geometry=lau_regions.geometry[supply_potentials.index],
     )
 
     heat_potentials_in_onshore_regions = gpd.sjoin(
@@ -148,20 +153,24 @@ if __name__ == "__main__":
 
     set_scenario_config(snakemake)
 
+    # get onshore regions and index them by region name
     regions_onshore = gpd.read_file(snakemake.input.regions_onshore).to_crs("EPSG:4326")
     regions_onshore.index = regions_onshore.name
     regions_onshore.drop(columns=["name"], inplace=True)
 
+    # get LAU regions and index them by LAU-ID
     lau_regions = gpd.read_file(
         f"{snakemake.input.lau_regions}!LAU_RG_01M_2019_3035.geojson",
         crs="EPSG:3035",
     ).to_crs("EPSG:4326")
     lau_regions.index = lau_regions.GISCO_ID
 
+    # temperature scenario that was assumed by Manz et al. when computing potentials is 65C (default) or 85C
     this_temperature_scenario = ISI_TEMPERATURE_SCENARIOS[
         snakemake.params.constant_temperature_celsius
     ]
 
+    # get heat potentials, index them by LAU-ID and get the geothermal potentials
     isi_heat_potentials = pd.read_excel(
         snakemake.input.isi_heat_potentials,
         sheet_name="Matching_results",
@@ -174,22 +183,23 @@ if __name__ == "__main__":
             "Unit",
         )
     ][0]
-
-    geothermal_potentials = isi_heat_potentials[
+    geothermal_supply_potentials = isi_heat_potentials[
         (
             f"Supply_potentials_{this_temperature_scenario}",
             GEOTHERMAL_SOURCE,
         )
     ].drop(index="Total")
 
-    if not geothermal_potentials.index.isin(lau_regions.index).all():
+    # check if all LAU regions in ISI heat potentials are present in LAU Regions data
+    if not geothermal_supply_potentials.index.isin(lau_regions.index).all():
         raise ValueError(
             "Some LAU regions in ISI heat potentials are missing from the LAU Regions data."
         )
 
+    # get heat source power by mapping heat potentials to onshore regions and scaling to from supply potentials to technical potentials
     heat_source_power = get_heat_source_power(
         regions_onshore=regions_onshore,
-        geothermal_potentials=geothermal_potentials,
+        supply_potentials=geothermal_supply_potentials,
         full_load_hours=FULL_LOAD_HOURS,
         input_unit=input_unit,
     )
