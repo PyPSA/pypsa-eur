5f6a9b505d0030bae006edda965e6ddc2e5b2053
daniel-rdt <daniel.ruedt@openenergytransition.org>
2025-02-07 09:39:38 +0100

    add fneum code suggestion

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -2153,10 +2153,9 @@ def add_heat(n: pypsa.Network, costs: pd.DataFrame, cop: xr.DataArray):
             )
 
             heat_dsm_profile = pd.read_csv(
-                snakemake.input.heat_dsm_profile, header=[1], index_col=[0]
-            )[nodes]
-            heat_dsm_profile.index = pd.DatetimeIndex(heat_dsm_profile.index)
-            heat_dsm_profile = heat_dsm_profile.reindex(n.snapshots)
+                snakemake.input.heat_dsm_profile, header=[1], index_col=[0], parse_dates=True,
+            )[nodes].reindex(n.snapshots)
+
             e_nom = (
                 heat_demand[["residential space"]]
                 .T.groupby(level=1)
