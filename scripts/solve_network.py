2f67385d16d543c1044338c1c1f952f411cf00d7
Tom Kaehler <tom_khlr@icloud.com>
2025-02-13 09:39:30 +0100

    add constraints

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -794,6 +794,58 @@ def add_operational_reserve_margin(n, sns, config):
     n.model.add_constraints(lhs <= rhs, name="Generator-p-reserve-upper")
 
 
+def add_tes_etpr_constraints(n):
+    """
+    Add an indexed constraint component that enforces for each TES storage unit that
+    the storage's energy capacity is at least the energy-to-power ratio times the corresponding
+    discharger link's power capacity.
+
+         Store-e_nom - etpr * Link-p_nom >= 0
+    """
+    tes_discharger_bool = n.links.index.str.contains("water tanks discharger|water pits discharger")
+    tes_discharger_ext = n.links[tes_discharger_bool].query("p_nom_extendable").index
+    if len(tes_discharger_ext) == 0:
+        return
+
+    etpr_values = n.links.loc[tes_discharger_ext, "etpr"].values
+
+    ltes_store = [link.replace(" discharger", "") for link in tes_discharger_ext]
+
+    ltes_e_nom = n.model["Store-e_nom"].loc[ltes_store]
+    discharger_pnoms = n.model["Link-p_nom"].loc[tes_discharger_ext]
+
+    lhs = ltes_e_nom - etpr_values * discharger_pnoms
+
+    n.model.add_constraints(lhs >= 0, name="TES_EtPR")
+
+
+def add_tes_charger_discharger_constraints(n):
+    """
+    Add constraints ensuring that for each TES unit, the charger and discharger are sized the same.
+
+        Link-p_nom(charger) - efficiency * Link-p_nom(discharger) == 0
+
+    This assumes that the discharger's efficiency (from n.links.efficiency) is used to scale the discharger power.
+    """
+    if not n.links.p_nom_extendable.any():
+        return
+
+    # Identify TES discharger and charger links using name patterns.
+    discharger_bool = n.links.index.str.contains("water tanks discharger|water pits discharger")
+    charger_bool = n.links.index.str.contains("water tanks charger|water pits charger")
+
+    # Filter only extendable links.
+    dischargers_ext = n.links[discharger_bool].query("p_nom_extendable").index
+    chargers_ext = n.links[charger_bool].query("p_nom_extendable").index
+
+    # Note: This works if the ordering of `chargers_ext` and `dischargers_ext` matches one-to-one.
+    # If not, you may need to build a mapping based on the names.
+    eff = n.links.efficiency[dischargers_ext].values
+    lhs = n.model["Link-p_nom"].loc[chargers_ext] - n.model["Link-p_nom"].loc[dischargers_ext] * eff
+
+    n.model.add_constraints(lhs == 0, name="tes_charger_discharger_ratio")
+
+
 def add_battery_constraints(n):
     """
     Add constraint ensuring that charger = discharger, i.e.
@@ -997,6 +1049,8 @@ def extra_functionality(
     ):
         add_solar_potential_constraints(n, config)
 
+    add_tes_charger_discharger_constraints(n)
+    add_tes_etpr_constraints(n)
     add_battery_constraints(n)
     add_lossy_bidirectional_link_constraints(n)
     add_pipe_retrofit_constraint(n)
