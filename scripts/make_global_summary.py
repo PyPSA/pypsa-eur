048781495189ce71e8500d7bb8dcf7653988c19e
Fabian Neumann <fabian.neumann@outlook.de>
2025-02-14 15:50:40 +0100

    fix mock_snakemake in make_global_summary

--- scripts/make_global_summary.py
+++ scripts/make_global_summary.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
 
-        snakemake = mock_snakemake("make_global_summary", kind="costs")
+        snakemake = mock_snakemake("make_global_summary")
 
     configure_logging(snakemake)
     set_scenario_config(snakemake)
