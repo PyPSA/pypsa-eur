079a589fc17f8f4e190cb353f0456b4102df835c
cristobal-GC <83344529+cristobal-GC@users.noreply.github.com>
2025-02-20 17:53:24 +0100

    small updates

--- scripts/build_electricity_demand_base.py
+++ scripts/build_electricity_demand_base.py
@@ -86,6 +86,61 @@ def upsample_load(
     return xr.concat(data_arrays, dim="bus")
 
 
+
+######################################## PyPSA-Spain
+#
+# Function to attach loads according to PyPSA-Spain methodology    ### EN PROCESO
+#
+def upsample_load_vPyPSA_Spain(
+    n: pypsa.Network,
+    regions_fn: str,
+    load_fn: str,
+    nuts3_fn: str,
+    distribution_key: dict[str, float],
+) -> pd.DataFrame:
+    substation_lv_i = n.buses.index[n.buses["substation_lv"]]
+    gdf_regions = gpd.read_file(regions_fn).set_index("name").reindex(substation_lv_i)
+    load = pd.read_csv(load_fn, index_col=0, parse_dates=True)
+
+    nuts3 = gpd.read_file(nuts3_fn).set_index("index")
+
+    gdp_weight = distribution_key.get("gdp", 0.6)
+    pop_weight = distribution_key.get("pop", 0.4)
+
+    data_arrays = []
+
+    for cntry, group in gdf_regions.geometry.groupby(gdf_regions.country):
+        load_ct = load[cntry]
+
+        if len(group) == 1:
+            factors = pd.Series(1.0, index=group.index)
+
+        else:
+            nuts3_cntry = nuts3.loc[nuts3.country == cntry]
+            transfer = shapes_to_shapes(group, nuts3_cntry.geometry).T.tocsr()
+            gdp_n = pd.Series(
+                transfer.dot(nuts3_cntry["gdp"].fillna(1.0).values), index=group.index
+            )
+            pop_n = pd.Series(
+                transfer.dot(nuts3_cntry["pop"].fillna(1.0).values), index=group.index
+            )
+
+            factors = normed(gdp_weight * normed(gdp_n) + pop_weight * normed(pop_n))
+
+        data_arrays.append(
+            xr.DataArray(
+                factors.values * load_ct.values[:, np.newaxis],
+                dims=["time", "bus"],
+                coords={"time": load_ct.index.values, "bus": factors.index.values},
+            )
+        )
+
+    return xr.concat(data_arrays, dim="bus")
+
+########################################
+
+
+
 if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
