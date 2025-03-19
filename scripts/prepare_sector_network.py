8ee9dc5da0f30c32062fab77d093947de363e28c
bobbyxng <bobbyxng@gmail.com>
2025-03-19 13:28:46 +0100

    Bug fix CO2 sequestration potentials.

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -655,7 +655,7 @@ def add_co2_tracking(n, costs, options, sequestration_potential_file=None):
         Configuration options containing at least:
         - regional_co2_sequestration_potential: dict with keys
             - enable: bool
-            - max_size: float
+            - max_size: float (in Mt)
             - years_of_storage: float
         - co2_sequestration_cost: float
         - co2_sequestration_lifetime: float
@@ -742,9 +742,7 @@ def add_co2_tracking(n, costs, options, sequestration_potential_file=None):
                 "sequestration_potential_file must be provided when "
                 "regional_co2_sequestration_potential is enabled"
             )
-        upper_limit = (
-            options["regional_co2_sequestration_potential"]["max_size"] * 1e3
-        )  # Mt
+        upper_limit = options["regional_co2_sequestration_potential"]["max_size"]  # Mt
         annualiser = options["regional_co2_sequestration_potential"]["years_of_storage"]
         e_nom_max = pd.read_csv(sequestration_potential_file, index_col=0).squeeze()
         e_nom_max = (
