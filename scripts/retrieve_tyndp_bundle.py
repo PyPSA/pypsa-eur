fb8857c96ec18dc40bf707bd922bbba0ddcf2f4a
Thomas Gilon <thomas.gilon@openenergytransition.org>
2025-05-26 15:19:23 +0200

    refactor: switch to original data source for reference grid data

--- scripts/retrieve_tyndp_bundle.py
+++ scripts/retrieve_tyndp_bundle.py
@@ -20,30 +20,26 @@ subdirectory, such that all files of the bundle are stored in the
 
 import logging
 import os
+import shutil
 import zipfile
+from pathlib import Path
 
 from _helpers import configure_logging, progress_retrieve, set_scenario_config
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
 # Define the base URL
-url = "https://zenodo.org/records/14230568/files/TYNDP_2024_data_bundle.zip"
+url_electricity = (
+    "https://2024-data.entsos-tyndp-scenarios.eu/files/scenarios-inputs/Line-data.zip"
+)
+url_buses = (
+    "https://2024-data.entsos-tyndp-scenarios.eu/files/scenarios-inputs/Nodes.zip"
+)
 
-if __name__ == "__main__":
-    if "snakemake" not in globals():
-        from _helpers import mock_snakemake
 
-        snakemake = mock_snakemake("retrieve_tyndp_bundle")
-        rootpath = ".."
-    else:
-        rootpath = "."
-
-    configure_logging(snakemake)
-    set_scenario_config(snakemake)
-    disable_progress = snakemake.config["run"].get("disable_progressbar", False)
-
-    to_fn = snakemake.output.dir
-    to_fn_zp = to_fn + ".zip"
+def retrieve_bundle(url: str, to_fn: str, disable_progress: bool = False):
+    to_fn_zp = Path(to_fn, Path(url).name)
 
     # download .zip file
     logger.info(f"Downloading TYNDP data bundle from '{url}'.")
@@ -54,7 +50,37 @@ if __name__ == "__main__":
     with zipfile.ZipFile(to_fn_zp, "r") as zip_ref:
         zip_ref.extractall(to_fn)
 
-    # remove .zip file
+    # remove .zip file and __MACOSX
     os.remove(to_fn_zp)
+    shutil.rmtree(Path(to_fn, "__MACOSX"))
+
+
+if __name__ == "__main__":
+    if "snakemake" not in globals():
+        from _helpers import mock_snakemake
+
+        snakemake = mock_snakemake("retrieve_tyndp_bundle")
+
+    configure_logging(snakemake)
+    set_scenario_config(snakemake)
+    disable_progress = snakemake.config["run"].get("disable_progressbar", False)
+
+    to_fn = Path(snakemake.output.dir)
+    urls = [
+        url_electricity,
+        url_buses,
+    ]
+
+    # Retrieve TYNDP data
+    tqdm_kwargs = {
+        "ascii": False,
+        "unit": " bundle",
+        "total": len(urls),
+        "desc": "Retrieving TYNDP data bundle",
+        "disable": disable_progress,
+    }
+
+    for url in tqdm(urls, **tqdm_kwargs):
+        retrieve_bundle(url, to_fn, disable_progress)
 
     logger.info(f"TYNDP data bundle available in '{to_fn}'.")
