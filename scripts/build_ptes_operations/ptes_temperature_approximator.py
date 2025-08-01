8cadf34e48d5a6328b34be0b625b92384d3e5b1c
Amos Schledorn <a.schledorn@tu-berlin.de>
2025-08-01 09:34:14 +0200

    add operational mode

--- scripts/build_ptes_operations/ptes_temperature_approximator.py
+++ scripts/build_ptes_operations/ptes_temperature_approximator.py
@@ -3,6 +3,13 @@
 # SPDX-License-Identifier: MIT
 
 import xarray as xr
+from enum import Enum
+
+
+class OperationalMode(Enum):
+    """PTES operational modes."""
+    CONSTANT_TEMPERATURE = "constant temperature"
+    DYNAMIC_TEMPERATURE = "dynamic temperature"
 
 
 class PtesTemperatureApproximator:
@@ -22,6 +29,8 @@ class PtesTemperatureApproximator:
         Maximum operational temperature of top layer in PTES.
     min_ptes_bottom_temperature : float
         Minimum operational temperature of bottom layer in PTES.
+    operational_mode : OperationalMode
+        PTES operational mode.
     """
 
     def __init__(
@@ -31,6 +40,7 @@ class PtesTemperatureApproximator:
         max_ptes_top_temperature: float,
         min_ptes_bottom_temperature: float,
         charger_temperature_boosting_required: bool,
+        operational_mode: OperationalMode,
     ):
         """
         Initialize PtesTemperatureApproximator.
@@ -47,38 +57,56 @@ class PtesTemperatureApproximator:
             Minimum operational temperature of bottom layer in PTES.
         charger_temperature_boosting_required : bool,
             Clip forward_temperature at max_ptes_top_temperature if True.
+        max_ptes_top_temperature : float, optional
+            Maximum operational temperature of top layer in PTES, default 90°C.
+        min_ptes_bottom_temperature : float, optional
+            Minimum operational temperature of bottom layer in PTES, default 35°C.
+        operational_mode : OperationalMode, optional
+            PTES operational mode, default VARIABLE_TEMPERATURE.
         """
         self.forward_temperature = forward_temperature
         self.return_temperature = return_temperature
         self.max_ptes_top_temperature = max_ptes_top_temperature
         self.min_ptes_bottom_temperature = min_ptes_bottom_temperature
         self.charger_temperature_boosting_required = charger_temperature_boosting_required
+        self.operational_mode = operational_mode
 
     @property
     def top_temperature(self) -> xr.DataArray:
         """
-        Forward temperature clipped at the maximum PTES temperature.
+        Forward temperature clipped at the maximum PTES temperature or constant max temperature.
 
         Returns
         -------
         xr.DataArray
             The resulting top temperature profile for PTES.
         """
-        return self._get_effective_forward_temperature(
-            self.forward_temperature
-        ).clip(max=self.max_ptes_top_temperature)
+        if self.operational_mode == OperationalMode.CONSTANT_TEMPERATURE:
+            return xr.full_like(self.forward_temperature, self.max_ptes_top_temperature)
+        elif self.operational_mode == OperationalMode.DYNAMIC_TEMPERATURE:
+            return self.forward_temperature.where(
+                self.forward_temperature <= self.max_ptes_top_temperature,
+                self.max_ptes_top_temperature,
+            )
+        else:
+            raise NotImplementedError(f"Operational mode {self.operational_mode} not implemented")
 
     @property
     def bottom_temperature(self) -> xr.DataArray:
         """
-        Return temperature clipped at the minimum PTES temperature.
+        Return temperature clipped at the minimum PTES temperature or constant min temperature.
 
         Returns
         -------
         xr.DataArray
             The resulting bottom temperature profile for PTES.
         """
-        return self.min_ptes_bottom_temperature
+        if self.operational_mode == OperationalMode.CONSTANT_TEMPERATURE:
+            return xr.full_like(self.return_temperature, self.min_ptes_bottom_temperature)
+        elif self.operational_mode == OperationalMode.DYNAMIC_TEMPERATURE:
+            return self.min_ptes_bottom_temperature
+        else:
+            raise NotImplementedError(f"Operational mode {self.operational_mode} not implemented")
 
     @property
     def e_max_pu(self) -> xr.DataArray:
