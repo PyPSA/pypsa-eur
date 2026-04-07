5730d58910970d35351486b80c4915ffe614218d
Jonas Hoersch <jonas.hoersch@openenergytransition.org>
2026-04-08 00:59:31 +0200

    perf: use auto chunks and a single dask thread

--- scripts/build_daily_heat_demand.py
+++ scripts/build_daily_heat_demand.py
@@ -19,13 +19,13 @@ import logging
 import geopandas as gpd
 import numpy as np
 import xarray as xr
-from dask.distributed import Client, LocalCluster
 
 from scripts._helpers import (
     configure_logging,
     get_snapshots,
     load_cutout,
     set_scenario_config,
+    setup_dask,
 )
 
 logger = logging.getLogger(__name__)
@@ -43,8 +43,7 @@ if __name__ == "__main__":
     set_scenario_config(snakemake)
 
     nprocesses = int(snakemake.threads)
-    cluster = LocalCluster(n_workers=nprocesses, threads_per_worker=1)
-    client = Client(cluster, asynchronous=True)
+    dask_kwargs = setup_dask(nprocesses)
 
     cutout_name = snakemake.input.cutout
 
@@ -71,7 +70,7 @@ if __name__ == "__main__":
     heat_demand = cutout.heat_demand(
         matrix=M.T,
         index=clustered_regions.index,
-        dask_kwargs=dict(scheduler=client),
+        dask_kwargs=dask_kwargs,
         show_progress=False,
     ).sel(time=daily)
 
